12a39ff2e20ddbafc53e04f25c95fa2cd3e524cc
limzh <liminzhang1013@gmail.com>
2020-08-01 10:57:55 +0800

    update

--- KNN/KNN.md
+++ KNN/KNN.md
@@ -1,7 +1,42 @@
-# 机器学习实践：KNN-Algorithm
+# 机器学习实践记录：从KNN算法到KD树
 
 *Author: Limzh*
 
 ## Section1. 序言
 
-在模式识别中，k近邻算法是一个非参数统计学的的分类器。
+在模式识别中，k近邻算法是一个非参数统计学的的分类算法。它的基本原理是，
+
+> 给定一个数据集$P = \{p_1, p_2,...,p_{m-1}\}$，$p_i \in \mathbb{R}^n, P \in \mathbb{R}^m\times\mathbb{R}^n$，以及一个相应的标签集 $L = \{l_1,l_2,...,l_{m-1}\}, l_i \in \{0,1,...,q\}$，q为标签集中的类别数。输入一预测点 $p \in \mathbb{R}^n$, 找到 数据集中离p最近的k个点，并依据它们的标签值给出预测点的标签值，并输出。
+
+简而言之，KNN算法秉持着“物以类聚，人以群分”的自然产生的思想，不寻找数据的分布模式（这也是非参数的原因，无模型），而是通过数据集和预测点的距离关系输出预测点的标签。
+
+通过调用`sklearn`中的相关函数，我们可以直接窥得KNN算法的实现结果。
+
+## sklearn 中 KNN 算法
+
+### 1.1 导入库介绍
+
+```python
+import numpy as np
+import pandas as pandas
+import scipy
+import matplotlib.pyplt as plt 
+from matplotlib.colors import ListedColormap
+from scipy import stats
+from sklearn import neighbors, datasets
+from sklearn.datasets.samples_generator import make_classification 
+```
+
+numpy，pandas，scipy 和 matplotlib 是数据处理和可视化的标准库.
+sklearn是python的机器学习算法库之一，全称scikit-learn。它集成了四大类机器学习算法，包括分类，回归，降维和聚类。同时它还支持生成算法需要的标数据集，数据的预处理以及数据的引入等功能。`make_classification`就是一种生成样本数据的数据生成器。`neighbors`包装了大量最近邻有关算法。
+
+### 1.2 生成数据学习
+
+```python
+X, y = make_classification(n_)
+```
+
+## 附录
+
+### 1. [sklearn库的结构](https://blog.csdn.net/algorithmPro/article/details/103045824?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-3.channel_param&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-3.channel_param)
+
--- KNN/KNN.py
+++ KNN/KNN.py
@@ -4,7 +4,7 @@ import pandas as pd
 from matplotlib.colors import ListedColormap
 from sklearn import neighbors, datasets
 from sklearn.datasets.samples_generator import make_classification
-import kdtree_limzh as kd
+import kdTree_limzh as kd
 
 # X is a vector of x inputs, y is the output. 200 samples
 # For each sample, we have 2 features. The outputs are classified into 3 classes.
@@ -12,9 +12,9 @@ import kdtree_limzh as kd
 X, y = make_classification(n_samples = 200, n_features = 2, n_redundant = 0,
                             n_clusters_per_class=1, n_classes = 3)
 
-clf = neighbors.KNeighborsClassifier(n_neighbors=1, weights = 'distance')
+clf = neighbors.KNeighborsClassifier(n_neighbors=15, weights = 'distance')
 clf.fit(X,y)
-h = .02 # stepsize in mesh
+h = .05 # stepsize in mesh
 x_min, x_max = X[:,0].min() - 1, X[:,0].max() + 1
 y_min, y_max = X[:,1].min() - 1, X[:,1].max() + 1
 xx, yy = np.meshgrid(np.arange(x_min,x_max,h), np.arange(y_min, y_max, h))
@@ -27,14 +27,18 @@ Z = Z.reshape(xx.shape)
 
 plt.figure()
 plt.subplot(121)
+plt.title('standard-defined')
 plt.pcolormesh(xx,yy,Z,cmap = cmap_light)
 plt.scatter(X[:,0], X[:,1], c = y, cmap = cmap_bold)
 plt.xlim(x_min, x_max)
 plt.ylim(y_min, y_max)
 plt.subplot(122)
 
-
-
+clf1 = kd.KNN_Classifier(X, y)
+res = clf1.train(K = 15, X = X, y = y, h = h)
+res = res.reshape(xx.shape)
+plt.title('self-defined')
+plt.pcolormesh(xx, yy, res, cmap = cmap_light)
 plt.scatter(X[:,0], X[:,1], c = y, cmap = cmap_bold)
 plt.xlim(x_min, x_max)
 plt.ylim(y_min, y_max)
