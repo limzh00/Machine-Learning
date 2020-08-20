f9599fee89c183209c9e710193f2fbc32287cbfd
limzh <liminzhang1013@gmail.com>
2020-08-20 19:04:22 +0800

    Update LogisticRegression.py

--- Supervised-Learning/GeneralizedLinearModel/algorithm/LogisticRegression.py
+++ Supervised-Learning/GeneralizedLinearModel/algorithm/LogisticRegression.py
@@ -6,7 +6,30 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from tqdm import tqdm
 
+class LogisticRgression_simple(object):
+    def __init__(self):
+        self.X = None
+        self.y = None
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+    def predict(self, P):
+        pass
+    def score(self, X, y):
+        return 1
+
 def main():
     X, y = samples_generator.make_classification(n_samples = 10000, n_features = 4)
-    x_train, y_train, x_test, y_test = train_test_split(X, y, test_size = 0.2)
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+    clf_sklearn = LogisticRegression()
+    clf = LogisticRgression_simple()
+    # fit
+    clf_sklearn.fit(x_train, y_train)
+    clf.fit(x_train, y_train)
+    # score
+    print(clf_sklearn.score(x_test, y_test))
+    print(clf.score(x_test, y_test))
+
+if __name__ == '__main__':
+    main()
     
